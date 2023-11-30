3300286adf7a9969267837d05b25a35c281a24df
Aman Chaursiya <amanchaurasiya786@gmail.com>
2023-11-30 11:40:28 +0530

    gitignore configure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // require("dotenv").config({ path: "./env" });
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
@@ -6,7 +7,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO DB connection failed !!!", error);
+  });
 
 /*
 import  express  from "express";
